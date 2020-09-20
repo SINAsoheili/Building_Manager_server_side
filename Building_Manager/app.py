5d85025d9ed00674e7ed450d06416c180d191771
SINA <sinasoheili79@gmail.com>
2020-09-20 22:41:45 +0430

    handle unit db changes

--- Building_Manager/app.py
+++ Building_Manager/app.py
@@ -26,6 +26,30 @@ def edit_building():
     result = manipulate_database(cmd)
     return jsonify({'response':result})
 
+
+
+#unit-------------------------------------
+@app.route('/unit/add' , methods=['POST'])
+def add_unit():
+    cmd = 'INSERT INTO `unit` (`owner_name` , `phone` ,`unit_number` , `tag` , `building_id`) VALUES (\'%s\' , \'%s\' , %i , %i , %i)'%(request.form['owner_name']  , request.form['phone'] , int(request.form['unit_number']) , int(request.form['tag']) , int(request.form['building_id']))
+    result = manipulate_database(cmd)
+    return jsonify({'response':result})
+
+
+@app.route('/unit/remove' , methods=['POST'])
+def remove_unit():
+    cmd = 'DELETE FROM `unit` WHERE `building_id`=%i AND `unit_number`=%i'%(int(request.form['building_id']) , int(request.form['unit_number']))
+    result = manipulate_database(cmd)
+    return jsonify({'response':result})
+
+
+@app.route('/unit/edit' , methods=['POST'])
+def edit_unit():
+    cmd = 'UPDATE `unit` SET `owner_name`=\'%s\' , `phone`=\'%s\' ,`unit_number`=%i , `tag`=%i WHERE `building_id`=%i AND unit_number=%i'%(request.form['owner_name'] , request.form['phone'] , int(request.form['unit_number']) ,  int(request.form['tag']) , int(request.form['building_id']) , int(request.form['prev_unit_number']))
+    result = manipulate_database(cmd)
+    return jsonify({'response':result})
+
+
 #insert|delete|edit|db---------------------------
 def manipulate_database(query):
     db = connector.connect(host=HOST , user=USER , passwd=PASSWD , database=DB_NAME , auth_plugin=AUTH_PLUGIN)
